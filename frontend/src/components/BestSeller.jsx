057b6b7340646c6e7aee414424f17cc27f3fe35c
arpita-sinha-14 <sinhaaarpita147@gmail.com>
2026-06-08 23:29:00 +0530

    Added a file

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -15,7 +15,9 @@ const BestSeller = () => {
         <div className='text-center text-3xl py-8'>
             <Title text1={'BEST'} text2={'SELLER'}/>
             <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, eius corporis! Reprehenderit incidunt nam expedita quo, quibusdam minus velit tenetur.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.  
+            Eius corporis! Reprehenderit incidunt nam expedita quo,
+             quibusdam minus velit tenetur.
             </p>
 
         </div>      
