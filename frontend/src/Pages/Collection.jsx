29ee6be2ed8241acef2396c9de5deafb369fbd14
arpita-sinha-14 <sinhaaarpita147@gmail.com>
2026-06-10 23:43:09 +0530

    Another day with this Project

--- frontend/src/Pages/Collection.jsx
+++ frontend/src/Pages/Collection.jsx
@@ -7,15 +7,15 @@ const Collection = () => {
   const [showFilter, setShowFilter] = useState(false);
 
   return (
-    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t text-gray-300'>
       {/* Filter Options */}
-      <div className='min-w-60'>
+      <div className='min-w-60 text-gray-700'>
         <p className='my-2 text-xl flex items-center cursor-pointer gap-2'> FILTERS </p>
         {/* Category filter */}
         <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
           <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
           <div>
-            
+
           </div>
 
         </div>
