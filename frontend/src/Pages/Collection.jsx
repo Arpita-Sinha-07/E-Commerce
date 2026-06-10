d7a5af450b96f20f92530bc52a67a14e0c5e7e91
arpita-sinha-14 <sinhaaarpita147@gmail.com>
2026-06-10 21:28:47 +0530

    Another day with this Project

--- frontend/src/Pages/Collection.jsx
+++ frontend/src/Pages/Collection.jsx
@@ -1,8 +1,26 @@
+import { useContext, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
 
 const Collection = () => {
+
+  const { products } = useContext(ShopContext)
+  const [showFilter, setShowFilter] = useState(false);
+
   return (
-    <div>
-      
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+      {/* Filter Options */}
+      <div className='min-w-60'>
+        <p className='my-2 text-xl flex items-center cursor-pointer gap-2'> FILTERS </p>
+        {/* Category filter */}
+        <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
+          <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
+          <div>
+            
+          </div>
+
+        </div>
+
+      </div>
     </div>
   )
 }
--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -16,7 +16,7 @@ const NewsletterBox = () => {
         </p>
 
         <form className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3">
-            <input className="w-full sm:flex-1 outline-none" type="email" placeholder="Enetr your Email" required/>
+            <input className="w-full sm:flex-1 outline-none" type="email" placeholder="Enter your Email" required/>
             <button className="bg-black text-white text-xs px-10 py-4" type="submit ">SUBSCRIBE</button>
         </form>
       
