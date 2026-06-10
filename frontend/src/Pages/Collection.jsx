8d728d4f8a096af07bd546b5f876c4df441c847c
arpita-sinha-14 <sinhaaarpita147@gmail.com>
2026-06-11 00:38:01 +0530

    Another day with this Project

--- frontend/src/Pages/Collection.jsx
+++ frontend/src/Pages/Collection.jsx
@@ -1,19 +1,25 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffects } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import Title from '../Components/Title'
 import { assets } from '../assets/frontend_assets/assets'
+import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
 
   const { products } = useContext(ShopContext)
   const [showFilter, setShowFilter] = useState(false);
+  const [filterProducts, setFilterProducts] = useState([]);
+
+  useEffects(()=>{
+    setFilterProducts(products);
+  },[])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t text-gray-300'>
       {/* Filter Options */}
       <div className='min-w-60 text-gray-700'>
-        <p className='my-2 text-xl flex items-center cursor-pointer gap-2'> FILTERS 
-          <img className={`h-3`} src={assets.dropdown_icon} alt="" />
+        <p onClick = {() => setShowFilter(!showFilter)} className='my-2 sm:text-xl flex items-center cursor-pointer gap-2'> FILTERS 
+          <img className={`h-3 sm:hidden ${showFilter ?'rotate-90' : ''} `} src={assets.dropdown_icon} alt="" />
         </p>
         {/* Category filter */}
         <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
@@ -57,17 +63,32 @@ const Collection = () => {
               Winterwear
             </p>
           </div>
-        </div>
-
-        
-        
+        </div> 
+      </div>
+      {/* Right Side */}
+      <div className='flex-1'>
+      <div className='sm:text-3xl text-base flex justify-between mb-4'>
+          <Title text1={'ALL'} text2={' COLLECTIONS'}/>
+          {/* Product Sort */}
+          <select className='boder-2 border-gray-300 text-sm px-2'>
+            <option value="relevant">Sort By : Relevant</option>
+            <option value="low-high">Sort By : Low to High</option>
+            <option value="high-low">Sort By : High to Low</option>
+          </select>
       </div>
 
-      <div className='text-3xl text-center flex flex-col'>
-          <Title text1={'ALL'} text2={' COLLECTIONS'}/>
-        </div>
-      
+      {/* Map Products */}
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
+        {
+          filterProducts.map((item,index)=>(
+            <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+          ))
+        }
+      </div>
+  
     </div>
+    </div>
+
   )
 }
 
