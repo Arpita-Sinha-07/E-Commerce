c83736f2fee4b597eceaaa59f42e6f9554bf5a31
arpita-sinha-14 <sinhaaarpita147@gmail.com>
2026-06-21 21:48:23 +0530

    Added some another features

--- frontend/src/Pages/Collection.jsx
+++ frontend/src/Pages/Collection.jsx
@@ -6,7 +6,7 @@ import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
 
-  const { products } = useContext(ShopContext)
+  const { products, search, showSearch} = useContext(ShopContext)
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
@@ -34,6 +34,9 @@ const Collection = () => {
   const applyFilter = () => {
     let productsCopy = products.slice();
 
+    if(showSearch && search){
+      productsCopy = productsCopy.filter(item.name.toLowerCase().includes(search.toLowerCase()))
+    }
     if(category.length > 0){
       productsCopy = productsCopy.filter(item => category.includes(item.category));
     }
