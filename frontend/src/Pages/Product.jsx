c997e72bef614b140648a03da8d0754cccf370ed
arpita-sinha-14 <sinhaaarpita147@gmail.com>
2026-06-24 23:33:17 +0530

    Commit

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 
 const Product = () => {
@@ -7,11 +7,30 @@ const { productId} = useParams();
 const {products} = useContext(ShopContext);
 const [productData, setProductData] = useState(false);
 const [image, setImage] = useState('')
-  return (
-    <div>
-      
+
+const fetchProductData = async () => {
+  products.map((item)=>{
+    if(item._id === productId){
+      setProductData(item)
+      setImage(item.image[0])
+      return null;
+    }
+  })
+}
+
+useEffect(()=>{
+  fetchProductData();
+
+}, [productId])
+  
+
+
+return productData? (
+    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+      <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+      </div>      
     </div>
-  )
+  ) : <div className="opacity-0"></div>
 }
 
 export default Product
